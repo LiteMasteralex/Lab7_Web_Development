fcce34c41a726b8f39c19dfb2b6eb1cf2cc61a6f
LiteMasteralex <alexlara_97@hotmail.com>
2019-10-24 10:50:37 -0500

    Initial server configuration & intalling missing package

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let express = require("express");
+let morgan = require("morgan");
+
+let app = express();
+
+app.use(express.static('public'));
+app.use(morgan("dev"));
+
+app.listen("8080", function() {
+	console.log("Welcome to the Blog-Post server.")
+});
