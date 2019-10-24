891c3648798bbee922fb69a2e0b79981fdd72d16
LiteMasteralex <alexlara_97@hotmail.com>
2019-10-24 11:28:41 -0500

    Added first call to get blog-post data

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
 console.log("Script has been loaded")
+
+let init = function() {
+	$.ajax({
+		url: "/blog-posts", 
+		method: "GET",
+		dataType: "json",
+		success: function(response) {
+			console.log(response);
+		},
+		error: function(error) {
+			console.log(error);
+		}
+	})
+};
+
+init();
