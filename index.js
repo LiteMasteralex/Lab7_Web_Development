72e3bb4db7d1e7a5128e84ee66b5196c514d49bd
LiteMasteralex <alexlara_97@hotmail.com>
2019-10-27 09:48:53 -0600

    Added function to search by author and function to clear the search

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.get("/blog-posts", function(req, res) {
 	return res.status(200).json(posts);
 });
 
-app.get("/blog-post?author=value", function(req, res) {
-	let author = req.params.author
+app.get("/blog-post", function(req, res) {
+	let author = req.query.author
 	if (author == "") {
 		res.statusMessage = "No author was provided for the search";
 		return res.status(406).json({
--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,29 @@ console.log("Script has been loaded")
 let blog_posts = [];
 
 let updateSelect = function() {
+	$("select").empty();
+	$("select").append(`<option value="">Select Post</option>`)
 	blog_posts.forEach( function(post) {
 		let option = `<option value=${post.id}>${post.title.substr(0, 15)}...</option>`;
 		$("select").append(option);
 	});
 }
+
+let updatePots = function(posts) {
+	let mainCotainer = $("main");
+	mainCotainer.empty()
+	blog_posts = [];
+	posts.forEach(function(post) {
+		blog_posts.push(post);
+		let blog_t = `<div class="post_title"><h2>${post.title}</h2></div>`;
+		let blog_h = `<div class="blog_header">${blog_t}<div class="user_image"><img src="" alt="user_image"></div></div>`;
+		let blog_c = `<div class="blog_content"><p>${post.content}</p></div>`;
+		let blog_f = `<div class="blog_footer"><div>${post.publishDate}</div><div>${post.author}</div></div>`;
+		let newHtml = `<div class="blog_container">${blog_h}${blog_c}${blog_f}`;
+		mainCotainer.append(newHtml);
+	});
+	updateSelect();
+};
 
 let init = function() {
 	$.ajax({
@@ -14,21 +32,34 @@ let init = function() {
 		method: "GET",
 		dataType: "json",
 		success: function(response) {
-			response.forEach(function(post) {
-				blog_posts.push(post);
-				let blog_t = `<div class="post_title"><h2>${post.title}</h2></div>`;
-				let blog_h = `<div class="blog_header">${blog_t}<div class="user_image"><img src="" alt="user_image"></div></div>`;
-				let blog_c = `<div class="blog_content"><p>${post.content}</p></div>`;
-				let blog_f = `<div class="blog_footer"><div>${post.publishDate}</div><div>${post.author}</div></div>`;
-				let newHtml = `<div class="blog_container">${blog_h}${blog_c}${blog_f}`;
-				$("main").append(newHtml);
-			});
-			updateSelect();
+			updatePots(response);
 		},
 		error: function(error) {
 			console.log(error);
 		}
 	})
 };
+
+$("#searchAuthor").on("click", function() {
+	let author = $("#searchValue").val();
+	console.log(author)
+	$.ajax({
+		url: "/blog-post?author=" + author, 
+		method: "GET",
+		dataType: "json",
+		success: function(response) {
+			updatePots(response);
+		},
+		error: function(error) {
+			console.log(error);
+		}
+	});
+});
+
+$("#clearSearch").on("click", function() {
+	init();
+	$("#searchValue").val("");
+});
+
 
 init();
